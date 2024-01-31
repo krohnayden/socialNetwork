7bcbcdba6e09508990948c59be573e1b3f922ace
Ayden Krohn <krohnayden@gmail.com>
2024-01-31 13:49:59 -0600

    added all schemas to models and connected to mongoose in connection.js

---
+++ models/thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./reaction');
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 128,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: function (createdAt) {
+                return createdAt.toLocaleDateString('en-US', {
+                    month: '2-digit',
+                    day: '2-digit',
+                    year: 'numeric',
+                })
+            }
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [Reaction],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Thought model
+const Thought = model('thought', thoughtSchema);
