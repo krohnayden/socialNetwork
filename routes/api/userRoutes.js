ab8acdad0563f60cf65c977a8b49340f6757c573
Ayden Krohn <krohnayden@gmail.com>
2024-02-02 22:20:08 -0600

    deleted unnecessary files and made api routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+module.exports = router;
